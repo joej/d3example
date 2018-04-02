b0fd27d2061216b4016efc88f3333bb43df98a2f
Joe Judge <joe@intrusion.org, judge@mitre.org, joseph.judge.ctr@us.af.mil>
2018-04-02 17:28:22 -0400

    updated

--- example_5.js
+++ example_5.js
@@ -1,15 +1,30 @@
 
 
-    var radius = 25;
-    var svg = d3.select("svg#svg")
-        //.attr("height", $("#page-content-wrapper").height() )          // -- fit to the bootstrap col height
-        .attr("height", "600px")
-        .attr("width", $("#page-content-wrapper").width() )            // -- fit to the bootstrap col width
-        .attr("top", $("#page-content-wrapper").top )                  // -- locate at 0,0 of bootstrap col
-        .attr("left", $("#page-content-wrapper").left )                // -- locate at 0,0 of bootstrap col
-        .style("background-color", "ivory")
-        //.style("border", "1px solid red")
-        .style("position", "absolute");
+
+    function clickednode(d, i) {
+        console.log("clicked on '" + d.name + "' (" + i + ")" );
+    }
+
+    function dragstarted(d) {
+        d3.select(this).raise().classed("active", true);
+    }
+    function dragged(d) {
+        //d3.select(this).attr("x", d.x = d3.event.x).attr("y", d.y = d3.event.y);
+        d3.select(this)
+            .select("image")
+            .attr("x", d.x = d3.event.x - (d.width/2) )
+            .attr("y", d.y = d3.event.y - (d.height/2) );
+
+        d3.select(this)
+            .select("text")
+            .attr("x", d.x = d3.event.x)
+            .attr("y", d.y = d3.event.y + d.height)
+            .style("text-anchor", "middle");
+    }
+    function dragended(d) {
+        d3.select(this).classed("active", false);
+        console.log("DRAGEND @ " + d3.event.x + ", " + d3.event.y );
+    }
 
     function handlemouseover(d, i) {
         console.log('mouseover');
@@ -30,7 +45,7 @@
 
         // - resize
         $(window).resize(function() {
-            console.log('resize');
+            //console.log('resize');
             svg.attr("height", "600px")
                 .attr("width", $("#page-content-wrapper").width() ) // -- fit to the bootstrap col width
                 .attr("top", $("#page-content-wrapper").top )       // -- locate at 0,0 of bootstrap col
@@ -48,34 +63,55 @@
             console.log("data-ma-action:start = " + action );
             switch(action) {
                 case "clear-lines":
-                    d3.selectAll("circle").style('opacity', 0);
                     d3.selectAll("text").style('opacity', 0);
                     d3.selectAll("image").style('opacity', 0);
                     d3.selectAll("line").style('opacity', 0);
                     break;
                 case "show-lines":
-                    d3.selectAll("circle").style('opacity', 1);
                     d3.selectAll("text").style('opacity', 1);
                     d3.selectAll("image").style('opacity', 1);
 
-                    // - draw lines
-                    d3.selectAll("image")
-                        .each( function(d, i) {
-                            // - for each node/d
-                            for (i in d.talksto) {
-                                // - find all they talk to
-                                d3.selectAll("image#" + d.talksto[i] )
-                                    .each( function(dd, ii) {
-                                        // - plot line from d to other node
-                                        svg.append("line")
-                                            .style("stroke", "black")   // look up CRYPTO RISK
-                                            .attr("x1", d.midx)
-                                            .attr("y1", d.midy)
-                                            .attr("x2", dd.midx)
-                                            .attr("y2", dd.midy);
-                                    });
-                            }
-                        });
+                    var FOO = true;
+
+                    if (FOO) {
+                        gnodes
+                            .each( function(d, i) {
+                                // - for each node/d
+                                for (i in d.talksto) {
+                                    // - find all they talk to
+                                    d3.selectAll("image#" + d.talksto[i] )
+                                        .each( function(dd, ii) {
+                                            // - plot line from d to other node
+                                            svg.append("line")
+                                                .style("stroke", "black")   // look up CRYPTO RISK
+                                                .attr("x1", d.midx)
+                                                .attr("y1", d.midy)
+                                                .attr("x2", dd.midx)
+                                                .attr("y2", dd.midy);
+                                        });
+                                }
+                            });
+
+                    } else {
+                        // - draw lines
+                        d3.selectAll("image")
+                            .each( function(d, i) {
+                                // - for each node/d
+                                for (i in d.talksto) {
+                                    // - find all they talk to
+                                    d3.selectAll("image#" + d.talksto[i] )
+                                        .each( function(dd, ii) {
+                                            // - plot line from d to other node
+                                            svg.append("line")
+                                                .style("stroke", "black")   // look up CRYPTO RISK
+                                                .attr("x1", d.midx)
+                                                .attr("y1", d.midy)
+                                                .attr("x2", dd.midx)
+                                                .attr("y2", dd.midy);
+                                        });
+                                }
+                            });
+                    }
                     break;
             }
             console.log("data-ma-action:end = " + action);
@@ -83,7 +119,18 @@
         });
 
 
-        // the d3 "way" ---------------
+        // ----------------------------------------------- D3
+        var radius = 25;
+
+        var svg = d3.select("svg#svg")
+            //.attr("height", $("#page-content-wrapper").height() )          // -- fit to the bootstrap col height
+            .attr("height", "600px")
+            .attr("width", $("#page-content-wrapper").width() )            // -- fit to the bootstrap col width
+            .attr("top", $("#page-content-wrapper").top )                  // -- locate at 0,0 of bootstrap col
+            .attr("left", $("#page-content-wrapper").left )                // -- locate at 0,0 of bootstrap col
+            .style("background-color", "ivory")
+            .style("position", "absolute");
+
         var d3_node_data = [];
         for (i in newNodeData) {
             obj = newNodeData[i];
@@ -92,64 +139,35 @@
             d3_node_data.push( obj);
         }
         var plot_area = svg.append("g").attr("id", "plot-area").attr("transform", "translate(0,0)" );
-        var circles = plot_area.selectAll("circle").data(d3_node_data);
 
+        var gnodes = plot_area.selectAll('g')
+            .data(d3_node_data)
+            .enter().append('g')
+            .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended)
+            );
 
-        DOCIRCLES = false;
-        DOIMAGES = true;
+        gnodes.append("image")
+            .attr("xlink:href", function(d) { return d.img } )
+            .attr("x", function(d) { return d.midx - (d.width/2) } )
+            .attr("y", function(d) { return d.midy - (d.height/2) } )
+            .attr("id", function(d) { return d.id} );
 
-        if (DOIMAGES) {
-            // plot circles
-            circles.enter()
-                .append("image")
-                .attr("xlink:href", function(d) { return d.img } )
-                .attr("x", function(d) { return d.midx - (d.width/2) } )
-                .attr("y", function(d) { return d.midy - (d.height/2) } )
-                .attr("id", function(d) { return d.id} );
+        gnodes.append("text")
+            .attr("x", function(d) { return d.midx } )
+            .attr("y", function(d) { return d.midy + d.height } )
+            .text( function(d) { return d.id } )
+            .style("text-anchor", "middle");
 
-            // - mouse in/out
-            plot_area.selectAll("image")
-                .on("mouseover", handlemouseover).on("mouseout", handlemouseout)
-                .on("click", function(d) {
-                    // d = the dict for this clicked'd circle
-                    //d.pulse = !d.pulse;
-                    // DO SOMETHING
-                    console.log("clicked on '" + d.name + "'" );
-            });
+        gnodes
+            .on("mouseover", handlemouseover)
+            .on("mouseout", handlemouseout)
+            .on("click", clickednode );
 
-            // - add text
-            circles.enter()
-                .append("text")
-                .attr("dx", function(d) { return d.midx } )
-                .attr("dy", function(d) { return d.midy + d.height } )
-                .text( function(d) { return d.id } )
-                .style("text-anchor", "middle");
-        }
+        // ----------------------------------------------- D3
 
-        if (DOCIRCLES) {
-            circles.enter()
-                .append("circle")
-                .attr("cx", function(d) { return d.midx } )
-                .attr("cy", function(d) { return d.midy } )
-                .attr("r", radius)
-                .attr('id', function(d) { return d.id } )
-                .style("fill", "white")
-                .style("stroke", "blue")
-                .attr("stroke-width", 2);
-
-            plot_area.selectAll("circle")
-                .on("mouseover", handlemouseover).on("mouseout", handlemouseout)
-                .on("click", function(d) {
-                    // d = the dict for this clicked'd circle
-                    //d.pulse = !d.pulse;
-                    // DO SOMETHING
-                    console.log("clicked on '" + d.name);
-            });
-
-        }
-        // end the d3 "way" ---------------
-        //
-        //
 
         // - HTML, bootstrap ----------
         var architectures = [ 'curpeace', 'futpeace', 'posthab' ];
